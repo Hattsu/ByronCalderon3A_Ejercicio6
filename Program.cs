33dea26c4e3b92dacf227fb073bd2d33b864e995
Hattsu <102260446+Hattsu@users.noreply.github.com>
2022-05-26 23:06:05 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+namespace ByronCalderon3A_Ejercicio6
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Leer_numeros leer_numeros = new Leer_numeros();
+                leer_numeros.Calcular();
+            }
+        
+    }
+}
